3e90a6a2ed12d6e592cd92bcc93db308bb7fdf92
Mipo8159 <stalkerglac@gmail.com>
2020-09-28 16:43:31 +0400

    Updates

---
+++ precache-manifest.d4b0cebc80127e632d58fa306463fea9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42a72951cc7b4af1c17f233129dad996",
+    "url": "/monster-app/index.html"
+  },
+  {
+    "revision": "c707637b38d4cc9a9a74",
+    "url": "/monster-app/static/css/main.be0d4425.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-app/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-app/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c707637b38d4cc9a9a74",
+    "url": "/monster-app/static/js/main.da875709.chunk.js"
+  },
+  {
+    "revision": "92a25797d6571872c4c3",
+    "url": "/monster-app/static/js/runtime-main.79c229cc.js"
+  }
+]);
